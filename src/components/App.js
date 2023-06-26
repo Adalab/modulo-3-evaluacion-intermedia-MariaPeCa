fb6eb87124e8096bb9c717cc30f6d75f4adecfc0
MariaPeCa <mpenacantero@gmail.com>
2023-06-26 13:02:52 +0200

    correcciones en clase

--- src/components/App.js
+++ src/components/App.js
@@ -2,97 +2,79 @@
 
 //.- de React, de archivos propios, Sass, Images
 import '../styles/App.scss';
-import {useEffect, useState } from 'react';
-import data from '../data/data.json'
+import { useEffect, useState } from 'react';
+
 // FUNCIÓN DEL COMPONENTE
 function App() {
   /*
     Variables de estado, funciones manejadoras de eventos, variables, funcion handle 
   */
-const [objectList, setObjectList] = useState ([]);
-const [quoteSearch, setQuoteSearch] = useState ('');
-// SECCIÓN DE USEEFFECT (cuando carga la página)
+  const [objectList, setObjectList] = useState([]);
+  const [quoteSearch, setQuoteSearch] = useState(''); //recoge lo que escribe la usuaria
+  const [filterCharacter, setFilterCharacter] = useState('all');
+  // SECCIÓN DE USEEFFECT (cuando carga la página)
 
-useEffect(() => {
-  fetch(
-    'https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json'
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      const dataWithId = data.map((eachObject) => {
-        return {
-          id: crypto.randomUUID(),
-          quote:eachObject.quote,
-          character:eachObject.character,
-        };
-      });
-        setObjectList(dataWithId);
-          
+  useEffect(() => {
+    fetch(
+      'https://beta.adalab.es/curso-intensivo-fullstack-recursos/apis/quotes-friends-tv-v1/quotes.json'
+    )
+      .then((response) => response.json())
+      .then((objectList) => {
+
+        console.log(objectList)
+        setObjectList(objectList);
+
       });
 
-}, []);
+  }, []);
 
-const handleQuoteSearch = (ev) => {
+  const handleQuoteSearch = (ev) => {
+  console.log(ev.target.value)
   setQuoteSearch(ev.target.value);
-}
+  }
+  const handleCharacter = (ev) => {
+    setFilterCharacter(ev.target.value);
+  }
 
-const renderObjectList = () => {
+  const renderObjectList = () => {
+    return objectList
+    .filter((element)=> element.quote.toLowerCase().includes(quoteSearch.toLowerCase()))
 
-  const filteredQuoteList = objectList.filter((eachObject) => 
-  eachObject.quote.toLowerCase().includes(quoteSearch.toLowerCase())
-  );
+    .filter((element) => {
+      
+      /*
+      if(filterCharacter === "all"){
+        return true;//también podría poner return = element
+      }else{
+        return element.character === filterCharacter;
+      }
+      */
+    })
+    .map((element, index) => (<li key={index}>
+      <h2>{element.quote}</h2>
+      <span>{element.character}</span>
+    </li>));
+  };
 
-  return filteredQuoteList.map((eachObject, id) => 
-  <li key={eachObject.id} className="item-list">{eachObject.quote} - {eachObject.character}</li>)
-}
-  
   /* RETURN --> HTML */
   return (
     <div>
-      <header className='header'>
-        <h1 className='header__title'>Frases de Friends</h1>
-        <fieldset className='fieldset'>
-          <form>
-            <label htmlFor="filterquote">Filtrar por frase</label>
-            <input
-              className='header__search'
-              autoComplete='off'
-              type='search'
-              name='search'
-              placeholder='Filtrar por frase'
-              value={quoteSearch}
-              onInput={handleQuoteSearch}
-            />
-          </form>
-          </fieldset>
-
-        {/*
-          <label>Filtrar por personajes</label>
-          <select name="" 
-          id="" 
-          onChange="" 
-          value="">
-            <option 
-            value="" disabled>
-              Todos
-            </option>
-            <option value="R">Ross</option>
-            <option value="M">Monica</option>
-            <option value="J">Joey</option>
-            <option value="P">Phoeby</option>
-            <option value="C">Chandler</option>
-            <option value="R">Rachel</option>
-          </select>
-        
-  */}
-        <fieldset>
-          <ul>
-           {renderObjectList()}
-          </ul>
-        </fieldset>
-      </header>
+      <form>
+        <label htmlFor="">Filtrar por frase</label>
+        <input type="text" onInput={handleQuoteSearch}/>
+        <label >Filtrar por personaje</label>
+        <select name="" id="" onChange={handleCharacter}>
+          <option value="all">Todos</option>
+          <option value="Joey">Joey</option>
+          <option value="Phoeby">Phoeby</option>
+          <option value="Chandler">Chandler</option>
+          <option value="Rachel">Rachel</option>
+          <option value="Ross">Ross</option>
+          <option value="Monica">Monica</option>
+        </select>
+      </form>
+      <ul>{renderObjectList()}</ul>
     </div>
-
   )
 }
 
